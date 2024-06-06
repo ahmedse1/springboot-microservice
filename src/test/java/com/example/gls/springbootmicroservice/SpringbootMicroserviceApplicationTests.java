2aadb6402ba1777d9402eab4c4d5e6331e758c10
ahmedse1 <ahmedhassan3841@gmail.com>
2024-06-06 15:24:44 +0200

    initial commit

---
+++ src/test/java/com/example/gls/springbootmicroservice/SpringbootMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gls.springbootmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
